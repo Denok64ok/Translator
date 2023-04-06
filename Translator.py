9e53a7288209142220f2a285150b190432d1202a
Denis <yaden9483@gmail.com>
2023-04-06 14:57:52 +0500

    Аdding a GUI application (graphical interface)

--- Translator.py
+++ Translator.py
@@ -1,3 +1,5 @@
+from tkinter import *
+from tkinter import ttk
 from googletrans import Translator
 
 
@@ -16,3 +18,41 @@ class Language_Translator:
     def translate(self, text):
         translation = self.translator.translate(text, src=self.input_language, dest=self.output_language)
         return translation.text
+
+class GUI_application:
+    def __init__(self, window):
+        self.window = window
+        window.title("Переводчик")
+        window.geometry('700x450')
+        window.grid_columnconfigure(0, weight=1)
+
+        self.languages = ["Англиский", "Русский", "Немецкий", "Французкий"]
+
+        self.choice_input_language = ttk.Combobox(values=self.languages)
+        self.choice_input_language.place(x=40, y=50)
+
+        self.input_text = Text(window, width=35, height=15, wrap="word")
+        self.input_text.place(x=40, y=100)
+        self.input_text.focus()
+
+        self.input_scrollbar = Scrollbar(window, orient="vertical", command=self.input_text.yview)
+        self.input_scrollbar.place(x=310, y=100, height=245)
+
+        self.input_text["yscrollcommand"] = self.input_scrollbar.set
+
+        self.choice_output_language = ttk.Combobox(values=self.languages)
+        self.choice_output_language.place(x=350, y=50)
+
+        self.output_text = Text(window, width=35, height=15, wrap="word")
+        self.output_text.place(x=350, y=100)
+
+        self.output_scrollbar = Scrollbar(window, orient="vertical", command=self.output_text.yview)
+        self.output_scrollbar.place(x=620, y=100, height=245)
+
+        self.output_text["yscrollcommand"] = self.output_scrollbar.set
+
+        self.Trans = Language_Translator()
+
+        self.button = Button(window, text="Перевести", command= self.output_text.insert(0,self.Trans.translate(str(self.input_text.get))),
+                             font=("Times New Roman", 14))
+        self.button.place(x=300, y=350)
