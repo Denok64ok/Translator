4c2998fce6f8a9b8001e0390e5a78f19e9bc9a12
Denis <yaden9483@gmail.com>
2023-05-07 19:58:44 +0500

    Correction of the return value in the text_search method of the Technic_OCR class

--- Translator.py
+++ Translator.py
@@ -72,10 +72,9 @@ class Technic_OCR:
 
     def text_search(self):
         image = cv2.imread(self.photo)
-        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         custom_config = r'--oem 3 --psm 6'
-        return pytesseract.image_to_string(image, config=custom_config, lang=self.language)
+        text = pytesseract.image_to_string(image, config=custom_config, lang=self.language)
+        return text.replace("\n", " ")
 
 
 class Voice_Recognition:
