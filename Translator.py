d2697c230afd4500fdd92d2ec2a641258f05acb2
Denis <yaden9483@gmail.com>
2023-05-09 14:37:20 +0500

    Creating a GUI using qt5

--- Translator.py
+++ Translator.py
@@ -1,5 +1,4 @@
-from tkinter import *
-from tkinter import ttk
+from PyQt5 import QtCore, QtGui, QtWidgets
 import pyautogui
 import keyboard
 from googletrans import Translator
@@ -167,41 +166,147 @@ class Upload_clipboard:
         return clipboard.paste()
 
 
-class GUI_application:
-    def __init__(self, window):
-        self.window = window
-        window.title("Переводчик")
-        window.geometry('700x450')
-        window.grid_columnconfigure(0, weight=1)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(593, 403)
+        MainWindow.setMinimumSize(QtCore.QSize(593, 403))
+        MainWindow.setMaximumSize(QtCore.QSize(593, 403))
+        MainWindow.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0.172249, y1:0.205, x2:0.80226, y2:0.79, stop:0.0677966 rgba(4, 87, 120, 255), stop:0.926136 rgba(6, 145, 201, 255));")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(20, 20, 261, 361))
+        self.frame.setStyleSheet("background-color: rgba(53,203,204,0.6);")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.textEdit = QtWidgets.QTextEdit(self.frame)
+        self.textEdit.setGeometry(QtCore.QRect(63, 60, 191, 241))
+        self.textEdit.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border: 4px solid #35cbcc;\n"
+"border-radius: 10px;\n"
+"color: rgb(0, 0, 0);")
+        self.textEdit.setObjectName("textEdit")
+        self.comboBox = QtWidgets.QComboBox(self.frame)
+        self.comboBox.setGeometry(QtCore.QRect(150, 20, 69, 22))
+        self.comboBox.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.pushButton_3 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_3.setGeometry(QtCore.QRect(10, 240, 41, 41))
+        self.pushButton_3.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_4.setGeometry(QtCore.QRect(10, 180, 41, 41))
+        self.pushButton_4.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_5.setGeometry(QtCore.QRect(10, 120, 41, 41))
+        self.pushButton_5.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_6.setGeometry(QtCore.QRect(10, 60, 41, 41))
+        self.pushButton_6.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_10 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_10.setGeometry(QtCore.QRect(70, 20, 61, 21))
+        self.pushButton_10.setAutoFillBackground(False)
+        self.pushButton_10.setStyleSheet("background-color: rgb(53, 203, 225);")
+        self.pushButton_10.setObjectName("pushButton_10")
+        self.pushButton_18 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_18.setGeometry(QtCore.QRect(80, 310, 41, 41))
+        self.pushButton_18.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_18.setObjectName("pushButton_18")
+        self.pushButton_19 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_19.setGeometry(QtCore.QRect(140, 310, 41, 41))
+        self.pushButton_19.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_19.setObjectName("pushButton_19")
+        self.pushButton_20 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_20.setGeometry(QtCore.QRect(200, 310, 41, 41))
+        self.pushButton_20.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_20.setObjectName("pushButton_20")
+        self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_8.setGeometry(QtCore.QRect(300, 30, 41, 41))
+        self.pushButton_8.setStyleSheet("background-color: rgb(53, 203, 225);")
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.frame_2 = QtWidgets.QFrame(self.centralwidget)
+        self.frame_2.setGeometry(QtCore.QRect(360, 20, 211, 361))
+        self.frame_2.setStyleSheet("background-color: rgba(53,203,204,0.6);")
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.textEdit_3 = QtWidgets.QTextEdit(self.frame_2)
+        self.textEdit_3.setGeometry(QtCore.QRect(10, 60, 191, 241))
+        self.textEdit_3.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border: 4px solid #35cbcc;\n"
+"border-radius: 10px;\n"
+"color: rgb(0, 0, 0);")
+        self.textEdit_3.setObjectName("textEdit_3")
+        self.comboBox_3 = QtWidgets.QComboBox(self.frame_2)
+        self.comboBox_3.setGeometry(QtCore.QRect(70, 20, 69, 22))
+        self.comboBox_3.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.comboBox_3.setObjectName("comboBox_3")
+        self.pushButton_21 = QtWidgets.QPushButton(self.frame_2)
+        self.pushButton_21.setGeometry(QtCore.QRect(30, 310, 41, 41))
+        self.pushButton_21.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_21.setObjectName("pushButton_21")
+        self.pushButton_22 = QtWidgets.QPushButton(self.frame_2)
+        self.pushButton_22.setGeometry(QtCore.QRect(90, 310, 41, 41))
+        self.pushButton_22.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_22.setObjectName("pushButton_22")
+        self.pushButton_23 = QtWidgets.QPushButton(self.frame_2)
+        self.pushButton_23.setGeometry(QtCore.QRect(150, 310, 41, 41))
+        self.pushButton_23.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_23.setObjectName("pushButton_23")
+        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_9.setGeometry(QtCore.QRect(300, 170, 41, 41))
+        self.pushButton_9.setStyleSheet("background-color: rgb(53, 203, 225);\n"
+"color: rgb(255, 153, 0);")
+        self.pushButton_9.setObjectName("pushButton_9")
+        MainWindow.setCentralWidget(self.centralwidget)
 
-        self.languages = ["Англиский", "Русский", "Немецкий", "Французкий"]
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.choice_input_language = ttk.Combobox(values=self.languages)
-        self.choice_input_language.place(x=40, y=50)
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.comboBox.setItemText(0, _translate("MainWindow", "Аглийский"))
+        self.comboBox.setItemText(1, _translate("MainWindow", "Китайский"))
+        self.pushButton_3.setText(_translate("MainWindow", "Вставить"))
+        self.pushButton_4.setText(_translate("MainWindow", "Прочитать из файла"))
+        self.pushButton_5.setText(_translate("MainWindow", "голосовая запись"))
+        self.pushButton_6.setText(_translate("MainWindow", "ножницы"))
+        self.pushButton_10.setText(_translate("MainWindow", "Определить язык"))
+        self.pushButton_18.setText(_translate("MainWindow", "Озвучить"))
+        self.pushButton_19.setText(_translate("MainWindow", "Записать в файл"))
+        self.pushButton_20.setText(_translate("MainWindow", "Копировать"))
+        self.pushButton_8.setText(_translate("MainWindow", "Поменять языки"))
+        self.pushButton_21.setText(_translate("MainWindow", "Озвучить"))
+        self.pushButton_22.setText(_translate("MainWindow", "Записать в файл"))
+        self.pushButton_23.setText(_translate("MainWindow", "Копировать"))
+        self.pushButton_9.setText(_translate("MainWindow", "Перевести"))
 
-        self.input_text = Text(window, width=35, height=15, wrap="word")
-        self.input_text.place(x=40, y=100)
-        self.input_text.focus()
 
-        self.input_scrollbar = Scrollbar(window, orient="vertical", command=self.input_text.yview)
-        self.input_scrollbar.place(x=310, y=100, height=245)
-
-        self.input_text["yscrollcommand"] = self.input_scrollbar.set
-
-        self.choice_output_language = ttk.Combobox(values=self.languages)
-        self.choice_output_language.place(x=350, y=50)
-
-        self.output_text = Text(window, width=35, height=15, wrap="word")
-        self.output_text.place(x=350, y=100)
-
-        self.output_scrollbar = Scrollbar(window, orient="vertical", command=self.output_text.yview)
-        self.output_scrollbar.place(x=620, y=100, height=245)
-
-        self.output_text["yscrollcommand"] = self.output_scrollbar.set
-
-        self.Trans = Translator_Googletrans()
-
-        self.button = Button(window, text="Перевести",
-                             command=self.output_text.insert(0, self.Trans.translate(str(self.input_text.get))),
-                             font=("Times New Roman", 14))
-        self.button.place(x=300, y=350)
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
